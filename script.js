b17677394398e022ffc82b4aa79135e211f9c345
Jakub Ucinski <kuba200104@gmail.com>
2022-09-08 20:47:28 +0100

    const instead of var

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-var stopwatch = document.getElementById("time");
-var startDate = new Date("4 September 2020");
+const stopwatch = document.getElementById("time");
+const startDate = new Date("4 September 2020");
 
 function updateTimer() {
-  var nowTime = Date.now();
-  var timeDelta = nowTime - startDate;
+  const nowTime = Date.now();
+  const timeDelta = nowTime - startDate;
   stopwatch.innerHTML = convertUTCDeltaToTimeDiff(timeDelta);
 }
 
 function convertUTCDeltaToTimeDiff(timeDelta) {
-  var second = 1000;
-  var minute = second * 60;
-  var hour = 60 * minute;
-  var day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = 60 * minute;
+  const day = hour * 24;
 
-  var dayCount = Math.floor(timeDelta / day);
+  const dayCount = Math.floor(timeDelta / day);
   timeDelta -= dayCount * day;
-  var hourCount = Math.floor(timeDelta / hour);
+  const hourCount = Math.floor(timeDelta / hour);
   timeDelta -= hourCount * hour;
-  var minuteCount = Math.floor(timeDelta / minute);
+  const minuteCount = Math.floor(timeDelta / minute);
   timeDelta -= minuteCount * minute;
-  var secondCount = Math.floor(timeDelta / second);
+  const secondCount = Math.floor(timeDelta / second);
 
   return `${dayCount} dni, ${hourCount} godzin, ${minuteCount} minut, ${secondCount} sekund`;
 }
